2b2f3743462e5320ca503ca68ad23aeef715261c
Max Alekseenko <nnwen0609@gmail.com>
2024-07-26 19:09:24 +0200

    update logo and fix header height

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className="bg-white">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <Image src="/logo.svg" alt="Chainscout Logo" width={150} height={40} />
+      <div className="flex items-center h-20 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <Image src="/logo.svg" alt="Chainscout Logo" width={115} height={22} />
       </div>
     </header>
   );
