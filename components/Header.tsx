06f72f8cdda7a7fd683654beb545c45a7147abab
Max Alekseenko <nnwen0609@gmail.com>
2024-07-29 19:30:32 +0200

    fix header padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className="bg-white">
-      <div className="flex items-center h-20 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <div className="flex items-center h-20 max-w-[1376px] mx-auto py-6 sm:px-6 lg:px-10">
         <Image src="/logo.svg" alt="Chainscout Logo" width={115} height={22} />
       </div>
     </header>
