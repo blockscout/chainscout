61b63a0a150b79b16541d37606c50469178668ff
Max Alekseenko <nnwen0609@gmail.com>
2025-02-25 12:17:03 +0000

    allow any domain for images, make small fixes

--- next.config.mjs
+++ next.config.mjs
@@ -5,36 +5,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'uploads-ssl.webflow.com'
+        hostname: '**',
       },
-      {
-        protocol: 'https',
-        hostname: 'd2e6b8w31txsjq.cloudfront.net'
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.prod.website-files.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'docs.inkonchain.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'haust.network'
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'blockscout-icons.s3.us-east-1.amazonaws.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'blockscout-icons.s3.amazonaws.com'
-      }
     ]
   },
   webpack(config) {
