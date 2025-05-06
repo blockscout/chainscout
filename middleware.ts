62d1717cfcdcde5a1ec780e818907b24b280e2a9
Max Alekseenko <nnwen0609@gmail.com>
2025-05-06 17:06:07 +0200

    enable cors

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+export function middleware() {
+  const response = NextResponse.next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  return response;
+}
+
+export const config = {
+  matcher: '/api/:path*',
+};
